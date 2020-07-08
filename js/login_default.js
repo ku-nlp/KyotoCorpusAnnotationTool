4c164f1ec15f109ee92912d258d211998d0d63e5
Yuta Hayashibe <yuta@hayashibe.jp>
2020-07-08 14:31:34 +0900

    Store selected values

---
+++ js/login_default.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const prefix = 'login_value_';
+
+  Array.from(document.getElementsByTagName('select'), s => {
+    const key = prefix + s.name;
+    const current = localStorage.getItem(key);
+    if (current !== null) {
+      s.value = current;
+    }
+
+    s.addEventListener("change", e => {
+      console.log(e, e.target.value)
+      localStorage.setItem(key, e.target.value);
+    });
+  })
+});
