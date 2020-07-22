e50d718eb56131b402f12c82017cc8e5b088fec8
Frederic Bergeron <bergeron@nlp.ist.i.kyoto-u.ac.jp>
2020-07-22 15:46:00 +0900

    Fixed issue #3.
    Escaped special characters in JQuery selector.

---
+++ js/escapeSelector.js
@@ -0,0 +1,28 @@
+//
+// Code taken from https://github.com/jquery/jquery/blob/d0ce00cdfa680f1f0c38460bc51ea14079ae8b07/src/selector/escapeSelector.js
+//
+
+// CSS string/identifier serialization
+// https://drafts.csswg.org/cssom/#common-serializing-idioms
+var rcssescape = /([\0-\x1f\x7f]|^-?\d)|^-$|[^\x80-\uFFFF\w-]/g;
+
+function fcssescape( ch, asCodePoint ) {
+    if ( asCodePoint ) {
+
+        // U+0000 NULL becomes U+FFFD REPLACEMENT CHARACTER
+        if ( ch === "\0" ) {
+            return "\uFFFD";
+        }
+
+        // Control characters and (dependent upon position) numbers get escaped as code points
+        return ch.slice( 0, -1 ) + "\\" + ch.charCodeAt( ch.length - 1 ).toString( 16 ) + " ";
+    }
+
+    // Other potentially-special ASCII characters get backslash-escaped
+    return "\\" + ch;
+}
+
+jQuery.escapeSelector = function( sel ) {
+    return ( sel + "" ).replace( rcssescape, fcssescape );
+};
+
