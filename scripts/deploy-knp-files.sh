c50888d457e52533b2dbef9775b6dc7495bf6a27
nobu-g <ueda-n127@hotmail.co.jp>
2022-04-11 00:10:22 +0900

    refactor

--- scripts/deploy-knp-files.sh
+++ scripts/deploy-knp-files.sh
@@ -9,15 +9,16 @@ here="$(realpath "$(dirname "$0")")"
 
 for knp_file in "${knp_dir}"/*.knp; do
   doc_id="$(basename "${knp_file}" .knp)"
-  group_id="$(echo "${doc_id}" | cut -c 1-7)"
-  mkdir -p "${deploy_dir}/${group_id}"
-  cd "${deploy_dir}/${group_id}"
-  cp "${knp_file}" ./
-  perl "${here}/../cgi/manage.pl" "${doc_id}.knp"
-  tar zcf "${doc_id}.tar.gz" "${doc_id}"
-  rm -rf "${doc_id}"
-  rm -f "${doc_id}.knp"
-  mkdir -p "${doc_id}"
-  mv -f "${doc_id}.tar.gz" "${doc_id}"
-  echo "${USER}	$(date +%Y-%m-%d) 00:00" > "${doc_id}/dirinfo"
+  group_id="$(echo "${doc_id}" | cut -c 1-8)"
+  group_dir="${deploy_dir}/${group_id}"
+  mkdir -p "${group_dir}" || exit 1
+  cp "${knp_file}" "${group_dir}/${doc_id}.knp"
+  # create ${group_dir}/${doc_id} directory
+  (cd "${group_dir}" && perl "${here}/../cgi/manage.pl" "${doc_id}.knp")
+  doc_dir="${group_dir}/${doc_id}"
+  tar zcf "${group_dir}/${doc_id}.tar.gz" "${doc_dir}" && rm -rf "${doc_dir}"
+  rm -f "${group_dir}/${doc_id}.knp"
+  mkdir -p "${doc_dir}"
+  mv -f "${group_dir}/${doc_id}.tar.gz" "${doc_dir}/"
+  echo "${USER}	$(date +%Y-%m-%d) 00:00" > "${doc_dir}/dirinfo"
 done
