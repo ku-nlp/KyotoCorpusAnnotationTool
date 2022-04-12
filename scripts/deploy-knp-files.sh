e02b7eef96b7fd3121e51f851bc534678f7246de
nobu-g <ueda-n127@hotmail.co.jp>
2022-04-12 15:51:28 +0900

    fix a bug in scripts/deploy-knp-files.sh

--- scripts/deploy-knp-files.sh
+++ scripts/deploy-knp-files.sh
@@ -14,21 +14,20 @@ deploy-single-file() {
   group_dir="${DEPLOY_DIR}/${group_id}"
   mkdir -p "${group_dir}" || exit 1
   cp "${knp_file}" "${group_dir}/${doc_id}.knp"
+  cd "${group_dir}"  # current directory must be the group directory
   # create ${group_dir}/${doc_id} directory
-  (cd "${group_dir}" && perl "${PROJECT_DIR}/cgi/manage.pl" "${doc_id}.knp")
-  doc_dir="${group_dir}/${doc_id}"
-  tar zcf "${group_dir}/${doc_id}.tar.gz" "${doc_dir}" && rm -rf "${doc_dir}"
-  rm -f "${group_dir}/${doc_id}.knp"
-  mkdir -p "${doc_dir}"
-  mv -f "${group_dir}/${doc_id}.tar.gz" "${doc_dir}/"
-  echo "${USER}	$(date +%Y-%m-%d) 00:00" > "${doc_dir}/dirinfo"
+  perl "${PROJECT_DIR}/cgi/manage.pl" "${doc_id}.knp" && rm -f "${doc_id}.knp"
+  tar zcf "${doc_id}.tar.gz" "${doc_id}" && rm -rf "${doc_id}"
+  mkdir -p "${doc_id}"
+  mv -f "${doc_id}.tar.gz" "${doc_id}/"
+  echo "${USER}	$(date +%Y-%m-%d) 00:00" > "${doc_id}/dirinfo"
 }
 
 export -f deploy-single-file
 
-sudo chown -R "${USER}:user" "${DEPLOY_DIR}"/*
+# sudo chown -R "${USER}:user" "${DEPLOY_DIR}"/*
 
-find "${knp_dir}" -type f -name "*.knp" -print0 | xargs -0 -n 1 -P 24 -I{} bash -c 'deploy-single-file "{}"'
+find "${knp_dir}" -type f -name "*.knp" -print0 | xargs -0 -n 1 -P 24 -I{} bash -cx 'deploy-single-file "{}"'
 
 # Apache web server manipulates files as user "www-data"
-sudo chown -R www-data:www-data "${DEPLOY_DIR}"/*
+# sudo chown -R www-data:www-data "${DEPLOY_DIR}"/*
